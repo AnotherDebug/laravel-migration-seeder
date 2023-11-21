b401b598c8d5ae0c97418626f4fc78bbde44b527
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-21 16:11:33 +0100

    [ADD]: home php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center mt-5">Elenco Treni</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolor nihil quos reprehenderit eos! Eaque,
+            temporibus. Quibusdam corporis sequi, iste quia, natus, perferendis impedit dolor hic sed laboriosam distinctio
+            tempora.</p>
+    </div>
+@endsection
