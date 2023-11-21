0a22803babea6c0d3f84b51d08e28f57f916e487
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-21 16:22:21 +0100

    [ADD]: table bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,40 @@
     <div class="container">
         <h1 class="text-center mt-5">Elenco Treni</h1>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolor nihil quos reprehenderit eos! Eaque,
-            temporibus. Quibusdam corporis sequi, iste quia, natus, perferendis impedit dolor hic sed laboriosam distinctio
-            tempora.</p>
+        <div class="container-fluid">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Azienda</th>
+                        <th scope="col">Stazione di partenza</th>
+                        <th scope="col">Stazione di arrivo</th>
+                        <th scope="col">Orario di partenza</th>
+                        <th scope="col">Orario di arrivo</th>
+                        <th scope="col">Codice Treno</th>
+                        <th scope="col">Numero Carrozze</th>
+                        <th scope="col">in ritardo</th>
+                        <th scope="col">cancellato</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
     </div>
 @endsection
