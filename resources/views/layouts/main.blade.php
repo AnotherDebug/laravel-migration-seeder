66105ec6e19cf37e20304bc54c7a6ab4d12e1f7f
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-21 16:11:18 +0100

    [UPDATE]: main php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Treni @yield('title')</title>
-       <!-- Styles -->
-       @vite('resources/js/app.js')
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
-<body>
 
-    @include()
 
-    @yield()
+<main>
+    @yield('content')
+</main>
+
 
 </body>
+
 </html>
