ed61eecb89f8fac5af4bea447540949092fc0301
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-21 14:56:55 +0100

    [UPDATE]: migration trains table with columns

--- database/migrations/2023_11_21_132520_create_trains_table.php
+++ database/migrations/2023_11_21_132520_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->Integer('codice_treno')->unsigned();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('is_delayed')->default(false);
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
