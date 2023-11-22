c68bf3f4134e196f3f9e754be26520fa826b32c2
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-22 14:39:21 +0100

    [ADD]: faker generator e for loop

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,8 +14,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-       $train = new Train();
+        for ($i = 0; $i < 20; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->word(true);
+            $train->stazione_partenza = $faker->words(3, true);
+            $train->stazione_arrivo = $faker->words(3, true);
+            $train->orario_partenza = $faker->date('Y-m-d H:i:s');
+            $train->orario_arrivo = $faker->date('Y-m-d H:i:s');
+            $train->codice_treno = $faker->numberBetween(1, 10000);
+            $train->numero_carrozze = $faker->numberBetween(5, 10);
+            $train->is_delayed = $faker->boolean(10);
+            $train->is_deleted = $faker->boolean(5);
+            $train->save();
+        }
     }
 }
